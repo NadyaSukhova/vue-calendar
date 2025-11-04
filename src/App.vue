71a473ce35ac67d31cfc4a7c9ebf3bf7726e19aa
Nadya Sukhova <suhova1313@gmail.com>
2025-11-04 16:15:51 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './components/Calendar.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+
+    <main>
+      <Calendar />
+    </main>
+  </div>
+</template>
